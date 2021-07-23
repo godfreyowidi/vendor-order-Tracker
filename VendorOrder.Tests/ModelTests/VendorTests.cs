9933e2978f8eaa73af9229bd356584e0b6aeb1cd
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-23 22:43:37 +0300

    add pass test to return name property

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -10,10 +10,20 @@ namespace VendorOrder.Tests
   public class VendorTests
   {
     [TestMethod]
-    public void VendoConstructor_CreatesInstancesOfVendor_Vendor()
+    public void VendorConstructor_CreatesInstancesOfVendor_Vendor()
     {
       Vendor newVendor = new Vendor("test vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+
+      string result = newVendor.Name;
+
+      Assert.AreEqual(name, result);
     }
   }
 }
