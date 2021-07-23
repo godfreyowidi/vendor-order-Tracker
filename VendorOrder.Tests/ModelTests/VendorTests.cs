f33a3302a8bca6bfb010117345542237ec69ea90
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-23 22:36:24 +0300

    add pass test for vendor constructor

---
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrder.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrder.Tests
+{
+  
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendoConstructor_CreatesInstancesOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
